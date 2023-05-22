9fecc013dc6805807fb5f9d278cc036dafb53c93
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 16:08:15 +0400

    add code to Order_Controller class

--- src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Controllers/Order_Controller.java
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Controllers/Order_Controller.java
@@ -1,4 +1,26 @@
 package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Controllers;
 
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models.Order;
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Services.Order_Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "order")
 public class Order_Controller {
+    //    http://localhost:8080/order/getAll
+    @Autowired
+    Order_Service order_service;
+    @RequestMapping(value = "getAll", method = RequestMethod.GET)
+    public List<Order> getAllOrders() {
+
+        return order_service.getAllOrders();
+    }
+    @GetMapping(value = "getById")
+    public Order getOrderById(@RequestParam Long id) {
+
+        return order_service.getOrderById(id);
+    }
 }
