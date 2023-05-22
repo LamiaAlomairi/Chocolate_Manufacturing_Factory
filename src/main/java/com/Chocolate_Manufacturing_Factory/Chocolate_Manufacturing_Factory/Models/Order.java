94e9a32bf2f84455a121e2c4785b9dcc73c564b4
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 11:17:05 +0400

    Add attributes to class order

--- src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Models/Order.java
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Models/Order.java
@@ -1,4 +1,21 @@
 package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "order")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    Integer quantity;
+    String shipping_address;
+    
 }
