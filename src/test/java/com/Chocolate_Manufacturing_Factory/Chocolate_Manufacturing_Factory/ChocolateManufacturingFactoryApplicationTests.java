e6ff9e7918e990e9bf9119ed5c7109cf24836981
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 09:22:59 +0400

    create Chocolate_Manufacturing_Factory project and packages

---
+++ src/test/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/ChocolateManufacturingFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChocolateManufacturingFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
