7c27681c4a6d61eb9472abf8cdc498f98fe3da2f
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 14:20:00 +0400

    add code to User_Service class

--- src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Services/User_Service.java
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Services/User_Service.java
@@ -1,5 +1,23 @@
 package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Services;
 
-public class User_Service {
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models.User;
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Repositories.User_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class User_Service {
+    @Autowired
+    User_Repository user_repository;
+    public List<User> getAllUsers() {
+
+        return user_repository.findAll();
+    }
+
+    public User getUserById(Long id) {
+
+        return user_repository.findById(id).get();
+    }
 }
