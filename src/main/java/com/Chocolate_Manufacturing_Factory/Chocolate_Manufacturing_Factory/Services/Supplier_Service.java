a083baf3b986f2f3bdbaecfd2072fc56531ebbc1
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 14:17:01 +0400

    add code to Supplier_Service class

--- src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Services/Supplier_Service.java
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Services/Supplier_Service.java
@@ -1,5 +1,23 @@
 package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Services;
 
-public class Supplier_Service {
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models.Supplier;
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Repositories.Supplier_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class Supplier_Service {
+    @Autowired
+    Supplier_Repository supplier_repository;
+    public List<Supplier> getAllSuppliers() {
+
+        return supplier_repository.findAll();
+    }
+
+    public Supplier getSupplierById(Long id) {
+
+        return supplier_repository.findById(id).get();
+    }
 }
