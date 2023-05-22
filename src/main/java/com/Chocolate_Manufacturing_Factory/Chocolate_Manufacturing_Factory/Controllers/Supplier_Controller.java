9ed5601dfaef282159d183cfc4ca936e2a225551
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 18:04:52 +0400

    add code to Supplier_Controller class

--- src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Controllers/Supplier_Controller.java
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Controllers/Supplier_Controller.java
@@ -1,5 +1,27 @@
 package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Controllers;
 
-public class Supplier_Controller {
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models.Order;
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models.Supplier;
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Services.Order_Service;
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Services.Supplier_Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+@RestController
+@RequestMapping(value = "supplier")
+public class Supplier_Controller {
+    //    http://localhost:8080/supplier/getAll
+    @Autowired
+    Supplier_Service supplier_service;
+    @RequestMapping(value = "getAll", method = RequestMethod.GET)
+    public List<Supplier> getAllSuppliers() {
+
+        return supplier_service.getAllSuppliers();
+    }
+    @GetMapping(value = "getById")
+    public Supplier getSupplierById(@RequestParam Long id) {
+
+        return supplier_service.getSupplierById(id);
+    }
 }
