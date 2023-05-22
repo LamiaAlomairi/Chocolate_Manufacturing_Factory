b0d0cb56eeb9ed525121010d7c86014d99ccabe5
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 12:00:10 +0400

    create Factory_Repository interface with extends

---
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Repositories/Factory_Repository.java
@@ -0,0 +1,7 @@
+package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Repositories;
+
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models.Factory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Factory_Repository extends JpaRepository<Factory, Long> {
+}
