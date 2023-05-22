535fe1519b182f158ec1e7fdfeb8ea468763fb79
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 12:10:11 +0400

    create Supplier_Repository interface with extends

---
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Repositories/Supplier_Repository.java
@@ -0,0 +1,7 @@
+package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Repositories;
+
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Supplier_Repository extends JpaRepository<Supplier, Long> {
+}
