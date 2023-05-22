14123a713e408c25eaca69d99eb876b1aee39576
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 10:24:18 +0400

    Add attributes into Factory class

--- src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Models/Factory.java
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Models/Factory.java
@@ -1,4 +1,22 @@
 package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "factory")
 public class Factory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String location;
+    Integer capacity;
+
 }
