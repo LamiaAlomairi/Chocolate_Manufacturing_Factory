ea3f48847104fa8e2a283aeb9f3cd2670897f835
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 12:45:50 +0400

    create Employee_ٍService class

---
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Services/Employee_ٍService.java
@@ -0,0 +1,8 @@
+package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Employee_ٍService {
+
+}
