6f5d2b54ed435b742759f2f6cd999604c69bdcc8
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 10:32:12 +0400

    Add attributes to class Ingredient

--- src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Models/Ingredient.java
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Models/Ingredient.java
@@ -1,4 +1,22 @@
 package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "ingredient")
 public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    Integer quantity;
+    Double cost;
+
 }
