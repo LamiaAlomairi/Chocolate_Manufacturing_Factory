11abafbb2184acb98071b6d2cd578d36a365f63d
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 10:20:18 +0400

    Add attributes into chocolate class

--- src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Models/Chocolate.java
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Models/Chocolate.java
@@ -1,4 +1,25 @@
 package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "chocolate")
 public class Chocolate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String flavor;
+    Integer quantity;
+    Double price;
+    String manufacturingDate;
+    String expirationDate;
+
 }
