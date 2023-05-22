11cf29f0b91894755dcd4eb3d6480fb842a024ff
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 12:03:31 +0400

    create Ingredient_Repository interface with extends

---
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Repositories/Ingredient_Repository.java
@@ -0,0 +1,7 @@
+package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Repositories;
+
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models.Ingredient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Ingredient_Repository extends JpaRepository<Ingredient, Long> {
+}
