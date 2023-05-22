8bf1f823f8adfc55d3e3933c22c8b7af235da6c1
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 14:54:41 +0400

    add code to Factory_Controller class

--- src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Controllers/Factory_Controller.java
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Controllers/Factory_Controller.java
@@ -1,5 +1,25 @@
 package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Controllers;
 
-public class Factory_Controller {
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models.Factory;
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Services.Factory_Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+@RestController
+@RequestMapping(value = "factory")
+public class Factory_Controller {
+    //    http://localhost:8080/factory/getAll
+    @Autowired
+    Factory_Service factory_service;
+    @RequestMapping(value = "getAll", method = RequestMethod.GET)
+    public List<Factory> getAllFactories() {
+
+        return factory_service.getAllFactories();
+    }
+    @GetMapping(value = "getById")
+    public Factory getFactoryById(@RequestParam Long id) {
+
+        return factory_service.getFactoryById(id);
+    }
 }
