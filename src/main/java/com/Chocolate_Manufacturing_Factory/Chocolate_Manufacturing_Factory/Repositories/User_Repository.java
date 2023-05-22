e81713fb9609d0d0f33a4c0ecb8cd93440eabb36
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 12:12:12 +0400

    create User_Repository interface with extends

---
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Repositories/User_Repository.java
@@ -0,0 +1,7 @@
+package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Repositories;
+
+import com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface User_Repository extends JpaRepository<User, Long> {
+}
