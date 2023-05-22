34d08e3ea75db0aa022d8164d5c876c24fa96eb6
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 10:27:38 +0400

    Add attributes into class Employee

--- src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Models/Employee.java
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Models/Employee.java
@@ -1,4 +1,23 @@
 package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "employee")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    String designation;
+    Double salary;
+    String joiningDate;
+
 }
