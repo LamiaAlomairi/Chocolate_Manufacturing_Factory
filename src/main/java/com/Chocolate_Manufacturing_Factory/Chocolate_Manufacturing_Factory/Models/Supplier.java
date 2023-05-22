6e1dcccde09125533805119156c555a5a0d2efbb
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-22 23:48:59 +0400

    add relationship to Supplier class

--- src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Models/Supplier.java
+++ src/main/java/com/Chocolate_Manufacturing_Factory/Chocolate_Manufacturing_Factory/Models/Supplier.java
@@ -1,10 +1,12 @@
 package com.Chocolate_Manufacturing_Factory.Chocolate_Manufacturing_Factory.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -20,4 +22,7 @@ public class Supplier {
     Integer contactNumber;
     String email;
 
+    @OneToMany(mappedBy = "supplier")
+    @JsonIgnore
+    private List<Ingredient> ingredients;
 }
